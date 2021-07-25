5560afde57fa01b0362ad6b4c271dcddc0ae5a17
Aidan Amato <aidanamato@comcast.net>
2021-07-25 11:33:32 -0400

    add logic to display current day on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
-console.log($("#currentDay"));
+// get and format the current day
+var currentTimeObj = luxon.DateTime.now();
+var currentDay = currentTimeObj.toFormat("EEEE, MMMM d");
+
+// logic to create day suffixes (lumon does not provide these)
+if (currentDay.slice(-1) === "1") {
+  if (currentDay.slice(-2, -1) === "1") {
+    var daySuffix = "th";
+  } else {
+    var daySuffix = "st";
+  }
+} else if (currentDay.slice(-1) === "2") {
+  if (currentDay.slice(-2, -1) === "1") {
+    var daySuffix = "th";
+  } else {
+    var daySuffix = "nd";
+  }
+} else if (currentDay.slice(-1) === "3") {
+  if (currentDay.slice(-2, -1) === "1") {
+    var daySuffix = "th";
+  } else {
+    var daySuffix = "rd";
+  }
+} else {
+  var daySuffix = "th";
+}
+
+// display current day on page
+$("#currentDay").text(currentDay + daySuffix);
