0828744eeee89de108d1b7643aec24626fbd2556
Aidan Amato <aidanamato@comcast.net>
2021-07-25 12:56:45 -0400

    added color coding to represent task urgency

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,55 @@
-// get and format the current day
-var currentTimeObj = luxon.DateTime.now();
-var currentDay = currentTimeObj.toFormat("EEEE, MMMM d");
+var timeRefresh = function() {
+  // get and format the current day
+  var currentTimeObj = luxon.DateTime.now();
+  var currentDay = currentTimeObj.toFormat("EEEE, MMMM d");
 
-// logic to create day suffixes (lumon does not provide these)
-if (currentDay.slice(-1) === "1") {
-  if (currentDay.slice(-2, -1) === "1") {
-    var daySuffix = "th";
+  // logic to create day suffixes (lumon does not provide these)
+  if (currentDay.slice(-1) === "1") {
+    if (currentDay.slice(-2, -1) === "1") {
+      var daySuffix = "th";
+    } else {
+      var daySuffix = "st";
+    }
+  } else if (currentDay.slice(-1) === "2") {
+    if (currentDay.slice(-2, -1) === "1") {
+      var daySuffix = "th";
+    } else {
+      var daySuffix = "nd";
+    }
+  } else if (currentDay.slice(-1) === "3") {
+    if (currentDay.slice(-2, -1) === "1") {
+      var daySuffix = "th";
+    } else {
+      var daySuffix = "rd";
+    }
   } else {
-    var daySuffix = "st";
-  }
-} else if (currentDay.slice(-1) === "2") {
-  if (currentDay.slice(-2, -1) === "1") {
     var daySuffix = "th";
-  } else {
-    var daySuffix = "nd";
   }
-} else if (currentDay.slice(-1) === "3") {
-  if (currentDay.slice(-2, -1) === "1") {
-    var daySuffix = "th";
-  } else {
-    var daySuffix = "rd";
-  }
-} else {
-  var daySuffix = "th";
-}
 
-// display current day on page
-$("#currentDay").text(currentDay + daySuffix);
+  // display current day on page
+  $("#currentDay").text(currentDay + daySuffix);
+
+  // color code tasks
+  var currentHour = currentTimeObj.toFormat("ha")
+  var taskNameQry = $(".description");
+  
+  taskNameQry.each(function() {
+    var currentId = $(this).attr("id")
+    var timeSlotObj = luxon.DateTime.fromFormat(currentId, "ha");
+
+    if (timeSlotObj.c.hour < currentTimeObj.c.hour) {
+      $(this).addClass("past");
+    }
+
+    if (timeSlotObj.c.hour === currentTimeObj.c.hour) {
+      $(this).addClass("present");
+    }
+
+    if (timeSlotObj.c.hour > currentTimeObj.c.hour) {
+      $(this).addClass("future");
+    }
+  });
+};
+
+// check time every 10 minutes
+setInterval(timeRefresh(), 1000 * 600);
